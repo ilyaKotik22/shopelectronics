4687ec58ae888f08e4eb260bf52d86263e819f32
ilyakotik22 <ilyakotik2204@gmail.com>
2026-01-28 19:39:24 +0300

    fix ui

--- my-app/src/lib/utils.ts
+++ my-app/src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const safeFetch = async <T>(  model: { findMany: (args?:any) => Promise<T> },
+  args?: Parameters<typeof model.findMany>[0]) => {
+  try{
+    console.log()
+    return model.findMany(args)
+
+  }catch(error){
+    console.error(error)
+    return []
+  }
+  
+}
